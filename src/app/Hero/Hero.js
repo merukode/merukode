4b6db7875d6256e0b52f49016c0b92b01c5ad425
ulrmdhn <auliarama965@example.com>
2023-05-30 13:12:07 +0700

    update

--- src/app/Hero/Hero.js
+++ src/app/Hero/Hero.js
@@ -2,13 +2,18 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div className='text-center h-3/4 p-3 flex flex-col justify-center items-center gap-4'>
-        <h1 className='font-extrabold text-4xl text-white'>Hi, im Rama</h1>
+    <div className='text-center h-3/4 p-3 flex flex-col justify-center items-center gap-4 md:flex-row md:justify-evenly md:w-9/12 md:m-auto '>
+        <div className='md:text-left md:w-2/4 md:flex md:flex-col md:gap-5 '>
+        <h1 className='font-extrabold text-4xl text-white md:text-5xl'>Hi, im <span className='md:hidden'>Rama</span> <span className='hidden md:block'>Aulia Ramadhan</span></h1>
         <p className='text-gray-200/50'>I'm a Front-End Web Developer with a passion for creating visually appealing and user-friendly websites.</p>
         <div>
-        <button className='bg-white p-3 rounded-lg'>
-            <p className='text-[#282A2B] font-medium'>About Me !</p>
+        <button className='bg-white p-3 rounded-lg hover:bg-slate-200 hover:translate-y-2'>
+            <p className='text-[#282A2B] font-medium '>About Me !</p>
         </button>
+        </div>
+        </div>
+        <div className='hidden md:block'>
+            <img className='w-72 hover:translate-y-2 transition' src='https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/avatar.png'/>
         </div>
     </div>
   )
--- src/app/Navbar/Navbar.js
+++ src/app/Navbar/Navbar.js
@@ -4,11 +4,11 @@ function Navbar() {
   return (
     <div className='w-80 m-auto justify-between p-3 flex items-center text-white md:w-3/4'>
         <div>
-        <img className='w-12' src='https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/mini-avatar.png'/>
+        <img className='w-12 hover:translate-y-2 transition cursor-pointer' src='https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/mini-avatar.png'/>
         </div>
         <div className='flex gap-4'>
-        <p className='hidden md:block underline underline-offset-8'>Home</p>
-        <p className='underline underline-offset-8'>Projects</p>
+        <p className='hidden md:block underline underline-offset-1 hover:underline-offset-8 cursor-pointer'>Home</p>
+        <p className='underline transition delay-150 ease-in underline-offset-1 hover:underline-offset-8  cursor-pointer'>Projects</p>
         </div>
     </div>
   )
