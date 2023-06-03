05d48a142ab22f816e98aacdb45524aabd4db866
ulrmdhn <auliarama965@example.com>
2023-06-03 11:19:31 +0700

    update project page

--- src/app/Projects/page.js
+++ src/app/Projects/page.js
@@ -4,8 +4,9 @@ import Navbar from "../Navbar/Navbar";
 function page() {
   return (
     <>
-      <Navbar />
-      <div>page</div>
+      <div className="w-screen bg-cover bg-center bg-no-repeat h-screen bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
+        <Navbar />
+      </div>
     </>
   );
 }
