022bf722f4ab61a17e0022eafc8a2359c4f3260a
ulrmdhn <auliarama965@example.com>
2023-06-06 14:00:36 +0700

    update

--- src/app/Projects/page.js
+++ src/app/Projects/page.js
@@ -1,29 +1,35 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
-import items from '../project.json'
+import items from "../project.json";
 
 function page() {
   return (
     <>
       <div className="w-screen bg-cover bg-center bg-no-repeat h-screen bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
         <Navbar />
-
-      {
-        items.map( item => {
-          return(
-            <div className="flex">
-              <div className="flex flex-col w-72 gap-3 border-2 border-black bg-white">
-              <img className="w-full" src={item.img} alt="img"/>
-              <span className="p-3">
-              <p className="text-lg font-bold mb-2 text-[#282A2B]">{item.title}</p>
-              <p className="text-sm font-medium mb-2 text-gray-600">{item.desc}</p>
-              <button className="text-white bg-[#282A2B] px-5 py-2 rounded-md font-bold">Visit</button>
-              </span>
+        <h1 className="text-center text-2xl font-bold underline underline-offset-8">
+          My Projects
+        </h1>
+        <div className="flex flex-col gap-5 md:flex-row items-center md:justify-evenly mt-10">
+          {items.map((item) => {
+            return (
+              <div className="flex rounded-xl flex-col w-72 h-96 gap-3 bg-white">
+                <img className="w-full" src={item.img} alt="img" />
+                <span className="p-3">
+                  <p className="text-lg font-bold mb-2 text-[#282A2B]">
+                    {item.title}
+                  </p>
+                  <p className="text-sm font-medium mb-2 text-gray-600">
+                    {item.desc}
+                  </p>
+                  <button className="text-white hover:bg-gray-900 bg-[#282A2B] px-5 py-2 rounded-md font-bold">
+                    Visit
+                  </button>
+                </span>
               </div>
-            </div>
-          )
-        })
-      }
+            );
+          })}
+        </div>
       </div>
     </>
   );
