3fab6f1bf3f2c0ac0c366ae1d14285b2eb7346f2
ulrmdhn <auliarama965@example.com>
2023-06-06 10:14:40 +0700

    update

--- src/app/Projects/page.js
+++ src/app/Projects/page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import items from '../project.json'
 
 function page() {
   return (
@@ -7,7 +8,22 @@ function page() {
       <div className="w-screen bg-cover bg-center bg-no-repeat h-screen bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
         <Navbar />
 
-        
+      {
+        items.map( item => {
+          return(
+            <div className="container grid grid-cols-3 place-content-center align-middle justify-center items-center">
+              <div className="flex flex-col w-72 gap-3 border-2 border-black bg-white">
+              <img className="w-full" src={item.img} alt="img"/>
+              <span className="p-3">
+              <p className="text-lg font-bold mb-2 text-[#282A2B]">{item.title}</p>
+              <p className="text-sm font-medium mb-2 text-gray-600">{item.desc}</p>
+              <button className="text-white bg-[#282A2B] px-5 py-2 rounded-md font-bold">Visit</button>
+              </span>
+              </div>
+            </div>
+          )
+        })
+      }
       </div>
     </>
   );
