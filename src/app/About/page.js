cbbe721af1f904c327e48ac4f6d63a410ee602b7
merukode <auliarama965@gmail.com>
2024-05-06 09:02:38 +0700

    Update Data

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -23,7 +23,7 @@ function page() {
     'Inkscape',
   ];
   return (
-    <div className="w-screen h-full 3xl:h-screen bg-cover bg-center bg-no-repeat bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
+    <div className="w-screen h-screen 3xl:h-screen bg-cover bg-center bg-no-repeat bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
       <Navbar/>
 
       <div className='section p-3 lg:w-3/4 lg:m-auto lg:justify-around text-white'>
@@ -34,10 +34,10 @@ function page() {
             alt='Avatar Profile'
           />
         </aside>
-        <p className='mb-5'>As a Junior Front-End Web Developer with a passion for creating visually appealing and user friendly websites. I am always eager to learn new thing to improve my programming skills to make a meaningful impact in the industry.</p>
+        <p className='mb-5'>As a Front-End Web Developer with a passion for creating visually appealing and user friendly websites. I am always eager to learn new thing to improve my programming skills to make a meaningful impact in the industry.</p>
         <p className='mb-5'>For developing a website. I often use ReactJs and TailwindCss. I also use NodeJS and Axios quite often. I am excited to learn new things like React Native for android programming and Typescript.</p>
         <p className='mb-5'>Outside of programming, i really love listening to music. When i run out of ideas to create something, music always helps me find the idea.</p>
-        <p className='mb-5'>I alsro really like reading. Comics, novels, news, and others. Reading keeps me updated with new things, and make me learn many things. When i am bored i will also look for books that i can read.</p>
+        <p className='mb-5'>I also really like reading. Comics, novels, news, and others. Reading keeps me updated with new things, and make me learn many things. When i am bored i will also look for books that i can read.</p>
         <h1 className='underline underline-offset-8 text-2xl font-bold py-3'>
           Skills & Tools
         </h1>
--- src/app/Projects/page.js
+++ src/app/Projects/page.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import items from "../project.json";
+import { FaArrowCircleRight } from "react-icons/fa";
+import Link from "next/link";
+
 
 function page() {
   return (
     <>
-      <div className="w-screen bg-cover bg-center bg-no-repeat bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
+      <div className="w-screen bg-cover p-10 bg-center bg-no-repeat bg-[url('https://raw.githubusercontent.com/ulrmdhn/me.portoflio/master/src/img/bg-color-grid.png')]">
         <Navbar />
         <h1 className="text-center text-white text-2xl font-bold mb-10 underline underline-offset-8">
           My Projects
@@ -13,19 +16,20 @@ function page() {
         <div className="flex flex-col gap-5 flex-wrap md:flex-row items-center md:justify-center mt-10 lg:w-9/12 lg:m-auto">
           {items.map((item) => {
             return (
-              <div key={item.id} className="flex hover:translate-x-3 transition rounded-xl flex-col w-72 h-96 gap-3 bg-white">
+              <div key={item.id} className="flex transition rounded-xl flex-col w-72 h-96 gap-3 bg-white">
                 <img className="w-72 rounded-t-lg" src={item.img} alt="img" />
-                <span className="p-3">
+                <span className="p-1">
                   <p className="text-lg font-bold mb-2 text-[#282A2B]">
                     {item.title}
                   </p>
                   <div className="flex gap-2 flex-wrap">
                     {item.tech.map((tech, index) => <p className='p-2 border-2 border-gray text-black hover:bg-black hover:text-white' key={index}>{tech}</p>)}
                   </div>
-
-                  {/* <button className="text-white hover:bg-gray-900 bg-[#282A2B] px-5 py-2 rounded-md font-bold">
-                    Visit
-                  </button> */}
+                </span>
+                <span className="mt-5 flex px-5 justify-end">
+                <Link href={item.link} target='none'>
+                  <FaArrowCircleRight className="text-black text-4xl"/>
+                </Link>
                 </span>
               </div>
             );
